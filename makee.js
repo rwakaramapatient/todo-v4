78087b64127ec521280f47f87518fb9d4c47bac5
rwakaramapatient <thesecond1018@gmail.com>
2026-02-23 09:58:01 +0200

    Week 2 Day 5: filters + stats

--- makee.js
+++ makee.js
@@ -4,20 +4,42 @@ let input = document.getElementById("input");
 let addBtn = document.getElementById("addBtn");
 let list = document.getElementById("list");
 let clear = document.getElementById("Clear");
-if (!input || !addBtn ||!list|| !clear){
-    throw new Error("Missing element");
+let all = document.getElementById("all");
+let active = document.getElementById("active");
+let completed = document.getElementById("completed");
+let currentFilter =localStorage.getItem("todoFilter") || "all";
+let stats = document.getElementById("stats");
+if (!input || !addBtn || !list || !clear || !all || !active || !completed || !stats) {
+  throw new Error("Missing element");
 }
 let todos = JSON.parse(localStorage.getItem("myTodos")) || [];
 todos = todos.map(t => (typeof t === "string" ? { text: t, done: false } : t));
-render();
+
 const saveToDisk = () =>{
    const stringifiedData = JSON.stringify(todos);
    localStorage.setItem("myTodos",stringifiedData);
 }
 
 function render() {
+
+let filteredList;
+switch (currentFilter) {
+    case "active": 
+        filteredList = todos.filter(t => t.done !== true);;
+        break;
+case "completed" :
+    filteredList = todos.filter(t => t.done === true);
+    break;
+    default:
+        filteredList = todos;
+        break;
+}
+const allStats= todos.length;
+  const completedStats= todos.filter(t => t.done).length;;
+const activeStats = todos.filter(t => !t.done).length;
+stats.textContent = `Total = ${allStats}   Active : ${activeStats}  Completed : ${completedStats}`;   
 list.textContent = "";
-todos.forEach(task => {
+filteredList.forEach(task => {
     const newButton = document.createElement("button");
      const newItem = document.createElement("li");
      const span = document.createElement("span");
@@ -37,18 +59,34 @@ todos.forEach(task => {
         saveToDisk();
         render();
       })
-
+          newItem.appendChild(newButton);
+          newItem.appendChild(span);
+          newItem.appendChild(dltBtn);
+    list.appendChild(newItem);
+}); 
+}
 clear.addEventListener("click", () => {
 todos = todos.filter(todo =>!todo.done);
 saveToDisk();
 render();
 })
-          newItem.appendChild(newButton);
-          newItem.appendChild(span);
-          newItem.appendChild(dltBtn);
-    list.appendChild(newItem);
-});    
-}
+all.addEventListener("click",() => {
+   currentFilter = "all";
+   localStorage.setItem("todoFilter", currentFilter);
+    render();
+})
+
+active.addEventListener("click",() => {
+    currentFilter = "active";
+    localStorage.setItem("todoFilter", currentFilter);
+    render();
+})
+
+completed.addEventListener("click",() => {
+    currentFilter = "completed"
+    localStorage.setItem("todoFilter", currentFilter);
+    render();
+})
 const adding = () => {
       const plan = input.value.trim();
     if (input.value.trim() === "") {
